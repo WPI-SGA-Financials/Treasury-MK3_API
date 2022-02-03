fe7efe807d7508dfbd2772f5e9d50f0909cdae68
kcbimonte <kbimonte@gmail.com>
2022-02-02 20:59:11 -0500

    feat: Initial setup of Funding Request Accessor Test

--- src/test/java/edu/wpi/sga/treasury/application/accessor/FundingRequestAccessorImplTest.java
+++ src/test/java/edu/wpi/sga/treasury/application/accessor/FundingRequestAccessorImplTest.java
@@ -1,14 +1,31 @@
 package edu.wpi.sga.treasury.application.accessor;
 
+import edu.wpi.sga.treasury.application.util.GeneralHelperFunctions;
+import edu.wpi.sga.treasury.domain.repository.FundingRequestRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class FundingRequestAccessorImplTest {
+
+    FundingRequestAccessor accessor;
+
+    // Repositories
+    @Mock
+    private FundingRequestRepository fundingRequestRepository;
+
+    // Util
+    @Mock
+    private GeneralHelperFunctions generalHelperFunctions;
 
     @BeforeEach
     void setUp() {
+        accessor = new FundingRequestAccessorImpl(fundingRequestRepository, generalHelperFunctions);
     }
 
     @Test

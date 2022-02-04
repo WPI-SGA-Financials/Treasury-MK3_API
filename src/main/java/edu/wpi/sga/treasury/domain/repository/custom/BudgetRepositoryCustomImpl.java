0756c8dcca63921ec01796d25bd9df992b644e07
kcbimonte <kbimonte@gmail.com>
2022-02-03 20:45:26 -0500

    feat: Fiscal Class filtering for budgets

--- src/main/java/edu/wpi/sga/treasury/domain/repository/custom/BudgetRepositoryCustomImpl.java
+++ src/main/java/edu/wpi/sga/treasury/domain/repository/custom/BudgetRepositoryCustomImpl.java
@@ -1,10 +1,7 @@
 package edu.wpi.sga.treasury.domain.repository.custom;
 
 import edu.wpi.sga.treasury.api.contract.request.PagedRequest;
-import edu.wpi.sga.treasury.domain.model.Budget;
-import edu.wpi.sga.treasury.domain.model.Budget_;
-import edu.wpi.sga.treasury.domain.model.Organization;
-import edu.wpi.sga.treasury.domain.model.Organization_;
+import edu.wpi.sga.treasury.domain.model.*;
 import edu.wpi.sga.treasury.domain.repository.custom.util.RepositoryHelperFunctions;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -31,7 +28,7 @@ public class BudgetRepositoryCustomImpl implements BudgetRepositoryCustom {
         orgJoin.alias("organizations");
 
         // Construct Filter
-        Predicate filter = getPredicate(request, cb, budget, orgJoin);
+        Predicate filter = getPredicate(request, cb, budget, orgJoin, query);
 
         List<Order> order = List.of(
                 cb.asc(orgJoin.get(Organization_.NAME)),
@@ -69,17 +66,67 @@ public class BudgetRepositoryCustomImpl implements BudgetRepositoryCustom {
         return entityManager.createQuery(countQuery).getSingleResult();
     }
 
-    private Predicate getPredicate(PagedRequest request, CriteriaBuilder cb, Root<Budget> budget, Join<Budget, Organization> budgetOrg) {
+    private Predicate getPredicate(PagedRequest request, CriteriaBuilder cb, Root<Budget> budget, Join<Budget, Organization> budgetOrg, CriteriaQuery<Budget> query) {
         RepositoryHelperFunctions helperFunctions = new RepositoryHelperFunctions(cb);
 
         List<Predicate> predicates = helperFunctions.getOrgBasedPredicate(request, budgetOrg);
 
-        if(!request.getFiscalYear().isEmpty()) {
+        if (!request.getFiscalYear().isEmpty()) {
             Path<String> path = budget.get(Budget_.FISCAL_YEAR);
 
             predicates.add(helperFunctions.filterEqual(request.getFiscalYear(), path));
         }
 
+        if (!request.getFiscalClass().isEmpty()) {
+            Subquery<Integer> subqueryLegacy = getLegacyFiscalClassSubquery(request, cb, budget, query);
+
+            Subquery<Integer> subquerySections = getSectionFiscalClassSubquery(request, cb, budget, query);
+
+            predicates.add(cb.or(cb.exists(subqueryLegacy), cb.exists(subquerySections)));
+        }
+
         return predicates.stream().reduce(cb::and).orElse(cb.conjunction());
+    }
+
+    private Subquery<Integer> getLegacyFiscalClassSubquery(PagedRequest request, CriteriaBuilder cb, Root<Budget> budget, CriteriaQuery<Budget> query) {
+        Subquery<Integer> subqueryLegacy = query.subquery(Integer.class);
+
+        Root<BudgetLegacy> legacySubRoot = subqueryLegacy.from(BudgetLegacy.class);
+
+        // Select Budget ID
+        subqueryLegacy.select(legacySubRoot.get(BudgetLegacy_.BUDGET).get(Budget_.ID));
+
+        subqueryLegacy.where(cb.and(
+                cb.function("fnc_FiscalClass",
+                                String.class,
+                                legacySubRoot.get(BudgetLegacy_.AMOUNT_PROPOSED),
+                                legacySubRoot.get(BudgetLegacy_.APPROVED_APPEAL))
+                        .in(request.getFiscalClass()),
+                cb.equal(legacySubRoot.get(BudgetLegacy_.BUDGET).get(Budget_.ID), budget.get(Budget_.ID))
+        ));
+
+        return subqueryLegacy;
+    }
+
+    private Subquery<Integer> getSectionFiscalClassSubquery(PagedRequest request, CriteriaBuilder cb, Root<Budget> budget, CriteriaQuery<Budget> query) {
+        Subquery<Integer> subquerySection = query.subquery(Integer.class);
+
+        Root<BudgetSection> sectionSubRoot = subquerySection.from(BudgetSection.class);
+        Join<BudgetSection, BudgetLineItem> lineItemJoin = sectionSubRoot.join(BudgetSection_.BUDGET_LINE_ITEMS);
+
+        // Select Budget ID
+        subquerySection.select(sectionSubRoot.get(BudgetSection_.BUDGET).get(Budget_.ID));
+
+        subquerySection.where(cb.equal(sectionSubRoot.get(BudgetLegacy_.BUDGET).get(Budget_.ID), budget.get(Budget_.ID)));
+
+        subquerySection.groupBy(sectionSubRoot.get(BudgetSection_.BUDGET));
+
+        subquerySection.having(cb.function("fnc_FiscalClass",
+                        String.class,
+                        cb.sum(lineItemJoin.get(BudgetLineItem_.AMOUNT_PROPOSED)),
+                        cb.sum(lineItemJoin.get(BudgetLineItem_.APPROVED_APPEAL)))
+                .in(request.getFiscalClass()));
+
+        return subquerySection;
     }
 }

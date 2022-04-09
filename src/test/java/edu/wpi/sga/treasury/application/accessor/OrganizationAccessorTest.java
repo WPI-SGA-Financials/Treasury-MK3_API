d9996df62906cb025e05a5a2eba85acd580fe336
Kevin C. Bimonte <kbimonte@gmail.com>
2022-04-09 11:50:57 -0400

    feat: Finish Organization Unit Testing (TR-83)

--- src/test/java/edu/wpi/sga/treasury/application/accessor/OrganizationAccessorTest.java
+++ src/test/java/edu/wpi/sga/treasury/application/accessor/OrganizationAccessorTest.java
@@ -1,5 +1,7 @@
 package edu.wpi.sga.treasury.application.accessor;
 
+import edu.wpi.sga.treasury.api.contract.request.PagedRequest;
+import edu.wpi.sga.treasury.application.dto.OrganizationDto;
 import edu.wpi.sga.treasury.application.util.GeneralHelperFunctions;
 import edu.wpi.sga.treasury.domain.model.Organization;
 import edu.wpi.sga.treasury.domain.repository.OrganizationRepository;
@@ -9,11 +11,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 
+import static edu.wpi.sga.treasury.application.accessor.test_utils.GeneralTestUtils.mockBasicFiltering;
+import static edu.wpi.sga.treasury.application.accessor.test_utils.GeneralTestUtils.mockBasicPagedRequest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -37,11 +44,51 @@ class OrganizationAccessorTest {
     @Test
     @DisplayName("Get Paged and Filtered list of Organizations")
     void getOrganizationsFiltered() {
+        // Arrange
+        mockBasicFiltering(generalHelperFunctions);
+
+        Organization org = new Organization();
+        org.setName("Cheese Club");
+        org.setAcronym("Cheese");
+        org.setClassification("Class 1 - Special Interest");
+
+        when(organizationRepository.findOrganizationsByFilters(any())).thenReturn(new PageImpl<>(List.of(org)));
+
+        // Act
+        PagedRequest request = new PagedRequest();
+        request.setName(List.of("Cheese Club", "Student", " "));
+
+        Page<OrganizationDto> returnedData = accessor.getOrganizations(request);
+
+        // Assert
+        assertEquals(1L, returnedData.getTotalElements());
+        assertEquals("Cheese Club", returnedData.getContent().get(0).getNameOfClub());
+        assertEquals("Cheese", returnedData.getContent().get(0).getAcronym());
     }
 
     @Test
     @DisplayName("Get Paged list of Organizations")
     void getOrganizations() {
+        // Arrange
+        mockBasicPagedRequest(generalHelperFunctions);
+
+        Organization org = new Organization();
+        org.setName("Cheese Club");
+        org.setAcronym("Cheese");
+        org.setClassification("Class 1 - Special Interest");
+
+        when(organizationRepository.findAllByInactiveIsFalse(any())).thenReturn(new PageImpl<>(List.of(org, org)));
+
+        // Act
+        PagedRequest request = new PagedRequest();
+        request.setPage(1);
+
+        Page<OrganizationDto> returnedData = accessor.getOrganizations(request);
+
+        // Assert
+        assertEquals(2L, returnedData.getTotalElements());
+        assertEquals("Cheese Club", returnedData.getContent().get(0).getNameOfClub());
+        assertEquals("Cheese", returnedData.getContent().get(0).getAcronym());
     }
 
     @Test
@@ -49,11 +96,18 @@ class OrganizationAccessorTest {
     void getOrganization() {
         // Arrange
         Organization org = new Organization();
+        org.setName("Cheese Club");
+        org.setAcronym("Cheese");
+        org.setClassification("Class 1 - Special Interest");
+
+        when(organizationRepository.findByName(any())).thenReturn(Optional.of(org));
 
         // Act
-
+        OrganizationDto returnedData = accessor.getOrganization("Cheese Club");
 
         // Assert
+        assertEquals("Cheese Club", returnedData.getNameOfClub());
+        assertEquals("Class 1 - Special Interest", returnedData.getClassification());
     }
 
     @Test
